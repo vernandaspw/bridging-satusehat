0b9ff34b0086edea7e4a367275f0ebf9082db22f
vernandaspw <vernandaspw@gmail.com>
2024-05-02 14:03:48 +0700

    aa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         // $schedule->command('encounter:rajal')->dailyAt('14:40');
-        $schedule->command('encounter:rajal')->everyMinute();
+        $schedule->command('encounter:rajal')->everyTwoMinutes();
     }
 
     /**
