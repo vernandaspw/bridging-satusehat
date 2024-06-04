75192140c49f0014e35b4d1f60107ecded0aa12c
vernandaspw <vernandaspw@gmail.com>
2024-06-04 09:41:23 +0700

    dashboard

--- app/Http/Livewire/DashboardPage.php
+++ app/Http/Livewire/DashboardPage.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 
 class DashboardPage extends Component
 {
+    public $qyear;
+    public $qmonth;
+    protected $queryString = [
+        'qyear' => ['except' => ''],
+        'qmonth' => ['except' => ''],
+    ];
     public function render()
     {
         return view('livewire.dashboard-page')->extends('layouts.app')->section('main');
@@ -14,22 +20,64 @@ class DashboardPage extends Component
 
     public function mount()
     {
-        $this->year = date('Y');
-        $this->month = date('m');
-
+        if ($this->qyear) {
+            $this->year = $this->qyear;
+        } else {
+            $this->year = date('Y');
+        }
+        if ($this->qmonth) {
+            $this->month = $this->qmonth;
+        } else {
+            $this->month = date('m');
+        }
+        $this->pilihBulan = $this->qmonth;
+        $this->pilihTahun = $this->qyear;
+        // dd($this->pilihBulan);
         $this->fetch();
+
+        $this->select_bulans = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $this->select_bulans[] = $i;
+        }
+
+        $this->select_tahuns = [];
+        $current_year = date("Y");
+        for ($i = 2023; $i <= $current_year; $i++) {
+            $this->select_tahuns[] = $i;
+        }
     }
+
+    public $select_bulans = [], $select_tahuns = [];
+
+    public $pilihBulan, $pilihTahun;
+
     public $year, $month;
 
     public $rajals = [];
 
     public function fetch()
     {
+        set_time_limit(9999);
         try {
+            // dd($this->year, $this->month);
             $this->rajals = RegistrationService::getCount($this->year, $this->month);
 
         } catch (\Throwable $e) {
             dd($e->getMessage());
         }
+    }
+
+    public function filter()
+    {
+        if (!$this->pilihBulan || !$this->pilihTahun) {
+            return $this->emit('error', 'pilih bulan & tahun');
+        }
+        $this->rajals = null;
+        $this->qmonth = $this->pilihBulan;
+        $this->qyear = $this->pilihTahun;
+        $this->month = $this->pilihBulan;
+        $this->year = $this->pilihTahun;
+
+        redirect('dashboard?qyear=' . $this->qyear . '&qmonth=' . $this->qmonth);
     }
 }
--- resources/views/livewire/dashboard-page.blade.php
+++ resources/views/livewire/dashboard-page.blade.php
@@ -15,13 +15,30 @@
                             <b>RAJAL</b>
                         </div>
                         <div class="d-flex">
-
+                            <form wire:submit.prevent='filter'>
+                                <select wire:model.defer='pilihBulan' id="">
+                                    <option value="">Pilih bulan</option>
+                                    @foreach ($select_bulans as $select_bulan)
+                                        <option value="{{ $select_bulan }}">{{ $select_bulan }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model.defer="pilihTahun" id="">
+                                    <option value="">Pilih tahun</option>
+                                    @foreach ($select_tahuns as $select_tahun)
+                                        <option value="{{ $select_tahun }}">{{ $select_tahun }}</option>
+                                    @endforeach
+                                </select>
+                                <button type="submit" class="btn bg-white ml-2">Filter</button>
+                                <div class="text-danger" wire:loading>Loading..</div>
+                            </form>
                         </div>
                     </div>
                     <div class="card-body">
-                        <div>
-                            <canvas id="myChart"></canvas>
-                        </div>
+                        @if ($rajals != null)
+                            <div>
+                                <canvas id="myChart"></canvas>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -29,44 +46,80 @@
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+    <script src="{{ asset('lib/sweatalert2/sweetalert2.all.min.js') }}"></script>
     <script>
-        const ctx = document.getElementById('myChart');
-        const chartDatas = @json($rajals);
-
-        const labels = chartDatas.map(e => e.tanggal);
-        const countIHS = chartDatas.map(entry => entry.countIHS);
-        const countNoIHS = chartDatas.map(entry => entry.countNoIHS);
-        const countTotal = chartDatas.map(entry => entry.countTotal);
-
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    type: 'bar',
-                    label: 'Terkirim',
-                    data: countIHS,
-                    borderColor: 'rgb(0, 255, 115)',
-                    backgroundColor: 'rgba(0, 255, 102)'
-                }, {
-                    type: 'bar',
-                    label: 'belum terkirim',
-                    data: countNoIHS,
-                    borderColor: 'rgb(255, 13, 0)',
-                    backgroundColor: 'rgba(255, 13, 0)'
-                }, {
-                    type: 'line',
-                    label: 'Total',
-                    data: countTotal,
-                    fill: false,
-                    borderColor: 'rgb(0, 110, 255)',
-                    backgroundColor: 'rgb(0, 110, 255)'
-                }]
-            },
-
-        });
+        Livewire.on('success', data => {
+            console.log(data);
+            Swal.fire({
+                position: 'center',
+                title: 'berhasil!',
+                text: data,
+                icon: 'success',
+                confirmButtonText: 'oke'
+                // showConfirmButton: false
+                // , timer: 1500
+            })
+        })
+        Livewire.on('error', data => {
+            console.log(data);
+            Swal.fire({
+                position: 'center',
+                title: 'gagal!',
+                text: data,
+                icon: 'error',
+                confirmButtonText: 'oke'
+                // showConfirmButton: false
+                // , timer: 1500
+            })
+        })
     </script>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    @if ($rajals != null)
+        <script>
+            document.addEventListener('livewire:load', function() {
+                let chart;
+                const ctx = document.getElementById('myChart');
+
+                renderChart(@json($rajals));
+
+                function renderChart(chartDatas) {
+                    const labels = chartDatas.map(e => e.tanggal);
+                    const countIHS = chartDatas.map(entry => entry.countIHS);
+                    const countNoIHS = chartDatas.map(entry => entry.countNoIHS);
+                    const countTotal = chartDatas.map(entry => entry.countTotal);
+
+                    chart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                type: 'bar',
+                                label: 'Terkirim',
+                                data: countIHS,
+                                borderColor: 'rgb(0, 255, 115)',
+                                backgroundColor: 'rgba(0, 255, 102)'
+                            }, {
+                                type: 'bar',
+                                label: 'belum terkirim',
+                                data: countNoIHS,
+                                borderColor: 'rgb(255, 13, 0)',
+                                backgroundColor: 'rgba(255, 13, 0)'
+                            }, {
+                                type: 'line',
+                                label: 'Total',
+                                data: countTotal,
+                                fill: false,
+                                borderColor: 'rgb(0, 110, 255)',
+                                backgroundColor: 'rgb(0, 110, 255)'
+                            }]
+                        },
+                    });
+
+                }
+
+            });
+        </script>
+    @endif
 </div>
